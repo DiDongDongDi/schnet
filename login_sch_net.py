379a68dae3e2646310366ad329a9ff9215af786a
kodyqywang <2572931653@qq.com>
2021-05-24 16:41:46 +0800

    做了一些优化

--- login_sch_net.py
+++ login_sch_net.py
@@ -1,29 +1,26 @@
-import requests 
+import requests
 
-try: 
-    requests.get('https://www.baidu.com/', timeout = 0.5)
-except requests.exceptions.ConnectTimeout:
+try:
+    requests.get('https://www.baidu.com/', timeout=0.5)
+except Exception:
     headers = {
         'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36'
-        }
+    }
 
     data = {
         'action': 'login',
-        'ac_id' : '3',
+        'ac_id': '3',
         'username': 'your_student_id',
         'password': 'your_password',
-        'save_me' : '1'
-        }
+        'save_me': '1'
+    }
 
-    url = 'http://172.20.20.1:801/srun_portal_pc.php?ac_id=3&' 
-
-    session = requests.Session()
-
-    session.post(url, headers=headers, data=data)
+    url = 'http://172.20.20.1:801/srun_portal_pc.php?ac_id=3&'
 
     try:
-        requests.get('https://www.baidu.com/', timeout = 0.5)
-    except requests.exceptions.ConnectTimeout:
+        requests.post(url, headers=headers, data=data)
+        requests.get('https://www.baidu.com/', timeout=0.5)
+    except Exception:
         print('Connect to the Internet failed !!!')
     else:
         print('Connect to the Internet successful !!!')
